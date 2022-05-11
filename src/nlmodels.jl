f4ed5392fc6e8a5c8fc08423d95a297bf30da33b
Matthias Sachs <msachs@math.ubc.ca>
2022-05-11 14:21:17 +0100

    update

--- src/nlmodels.jl
+++ src/nlmodels.jl
@@ -10,15 +10,31 @@ using NeighbourLists
 export FSModel
 
 struct FSModel
-    basis1
-    basis2
-    rcut
-    transform
-    c1
-    c2
+    basis1 # = B 
+    basis2 # = B'
+    rcut # cutoff radius of the ace bases B, B'
+    transform   # = - Sqrt 
+    transform_d # = 1 / (2 Sqrt)  
+    c1  # = a
+    c2  # = a'
 end
 
 function ACE.evaluate(m::FSModel, at)
+    nlist = neighbourlist(at, m.rcut)
+    lin_part, nonlin_part = 0.0,0.0
+    for k = 1:length(at)
+        _, Rs = NeighbourLists.neigs(nlist, k)
+        cfg = [ ACE.State(rr = r)  for r in Rs ] |> ACEConfig
+        B1 = ACE.evaluate(m.basis1, cfg) 
+        B2 = ACE.evaluate(m.basis2, cfg) 
+        lin_part += sum( c*b for (c,b) in zip(m.c1,B1)) 
+        nonlin_part += m.transform(sum( c*b for (c,b) in zip(m.c2,B2)).val)
+    end
+    return lin_part + nonlin_part
+end
+
+
+function ACE.evaluate_d(m::FSModel, at)
     nlist = neighbourlist(at, m.rcut)
     part1,part2 = 0.0,0.0
     for k = 1:length(at)
@@ -30,9 +46,8 @@ function ACE.evaluate(m::FSModel, at)
         part2 += m.transform(sum( c*b for (c,b) in zip(m.c2,B2)).val)
     end
     return part1 + part2
-
-
 end
+
 
 end
 #%%
