b41e353783fb7c15297d9f23ed3eb65138c8742c
Matthias Sachs <msachs@math.ubc.ca>
2022-06-01 15:39:05 +0100

    changes from meeting

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -1,56 +1,74 @@
 
+module Dynamics
+
 using ACE
 
 using JuLIP: AbstractAtoms
-abstract type Dynamics end
+using JuLIP: JVec
+
+using StaticArrays
+
 export run!
+
+
+abstract type Dynamics end
 """
-type "dynamics" must implement traverse!(at::AbstractAtoms; kwargs)
+type "Dynamics" must implement step!(at::AbstractAtoms; kwargs)
 """
-function run!(d::Dynamics, V, at::AbstractAtoms, N::Int)
-    for t = 1:N
+abstract type HamiltonianDynamics <: Dynamics end
+
+mutable struct VelocityVerlet{T} <: HamiltonianDynamics where {T}
+    F::Vector{JVec{T}} # force
+    h::Float64      # step size
+end
+
+VelocityVerlet(h::Float64, V, at::AbstractAtoms) = VelocityVerlet(forces(V, at), h)
+
+mutable struct PositionVerlet{T} <: HamiltonianDynamics where {T}
+    F::Vector{JVec{T}} # force
+    h::Float64      # step size
+end
+
+PositionVerlet(h::Float64, V, at::AbstractAtoms) = PositionVerlet(forces(V, at) , h) 
+
+B_step!(d::HamiltonianDynamics, at::AbstractAtoms; hf::T=1.0) where {T<:Real} = set_momenta!(at, at.P + hf * d.h * d.F)
+A_step!(d::HamiltonianDynamics, at::AbstractAtoms; hf::T=1.0) where {T<:Real} = set_positions!(at, at.X + hf * d.h * at.P./at.M)
+
+
+
+# outp = simpleoutp()
+# run!(d::Dynamics, V, at::AbstractAtoms, Nsteps::Int; outputscheduler = outp)
+# outp.x_traj
+
+function run!(d::Dynamics, V, at::AbstractAtoms, Nsteps::Int; outputscheduler = outp)
+    for t = 1:Nsteps
         step!(d::Dynamics, V, at)
+        feed!(d,V, at, outp)
     end
 end
-# run(d::dynamics,at::AbstractAtoms, Nsteps::i)
-abstract type GradientDynamics <: Dynamics end
 
-abstract type OLDDynamics <: GradientDynamics end
+abstract type outputscheduler end
 
-mutable struct EulerMaruyama{T} <: OLDDynamics where {T<:Real}
-    at::AbstractAtoms
-    h::T 
-    β::T    # step size
+begin struct simpleoutp
+    x_traj
+end
+simpleoutp() = simpleoutp([])
+function feed!(d,V, at, outp::simpleoutp)
+    push!(outp.x_traj,at.X)
 end
 
 function step!(d::EulerMaruyama, V, at::AbstractAtoms; hf::T=1.0) where {T}
     set_positions!(at, at.X + hf * d.h * forces(V, at)./at.M + sqrt.( hf/d.β * d.h/at.M).*randn(SVector{3,Float64},length(at)))
 end
 
-abstract type HamiltonianDynamics <: Dynamics end
-
-B_step!(d::HamiltonianDynamics, at::AbstractAtoms; hf::T=1.0) where {T<:Real} = set_momenta!(at, at.P + hf * d.h * d.forces)
-A_step!(d::HamiltonianDynamics, at::AbstractAtoms; hf::T=1.0) where {T<:Real} = set_positions!(at,at.X + hf * d.h * at.P./at.M)
 
 
-mutable struct VelocityVerlet{T} <: HamiltonianDynamics where {T}
-    at::AbstractAtoms
-    F::Vector{JVec{T}} # force
-    h::Float64      # step size
-end
-
-function step!(s::VelocityVerlet, V, at::AbstractAtoms ) #V::SitePotential
+function step!(s::VelocityVerlet, V, at::AbstractAtoms ) #V::SitePotential (e.g. FSModel)
     B_step!(s, at; hf=.5)
     A_step!(s, at; hf=1.0)
     s.F = forces(V, at)
     B_step!(s, at; hf=.5)
 end
-
-mutable struct PositionVerlet{T} <: HamiltonianDynamics where {T}
-    at::AbstractAtoms
-    F::Vector{JVec{T}} # force
-    h::Float64      # step size
- end
 
 function step!(s::PositionVerlet, V, at::AbstractAtoms ) #V::SitePotential
     A_step!(s, at; hf=.5)
@@ -59,40 +77,52 @@ function step!(s::PositionVerlet, V, at::AbstractAtoms ) #V::SitePotential
     A_step!(s, at; hf=.5)
 end
 
+abstract type GradientDynamics <: Dynamics end
+
+abstract type OLDDynamics <: GradientDynamics end
+
+mutable struct EulerMaruyama{T} <: OLDDynamics where {T<:Real}
+    h::T 
+    β::T    # step size
+ end
+
+function step!(d::EulerMaruyama, V, at::AbstractAtoms) where {T}
+    set_positions!(at, at.X + 1.0 * d.h * forces(V, at)./at.M + sqrt.( 1.0/d.β * d.h./at.M).*randn(ACE.SVector{3,Float64},length(at)))
+end
+
 abstract type Thermostat <: HamiltonianDynamics end
-"""
-must implement get_invtemp
-"""
 
 get_invtemp(d::Thermostat) = d.β
 
 abstract type Langevin <: Thermostat end 
 
-O_step!(s::Langevin, at::AbstractAtoms) = set_momenta!(at, s.α .* at.P + s.ζ * randn(SVector{3,Float64},length(at)) )
+O_step!(s::Langevin, at::AbstractAtoms) = set_momenta!(at, s.α .* at.P + s.ζ * randn(ACE.SVector{3,Float64},length(at)) )
 
-mutable struct BAOAB{T}  <: Langevin where {T<:Real}
-    h::T
-    forces::Vector{ACE.SVector{3,T}}
-    β::T
-    α::T
-    ζ::T
+mutable struct BAOAB{T} <: Langevin where {T<:Real}  
+    h::T        # Step size 
+    F::Vector{ACE.SVector{3,T}}
+    β::T        # Inverse Temperature 
+    α::T        # Integrator parameters
+    ζ::T        # Integrator parameters 
+
 end
 
-BAOAB(h::T, N::Int; γ::T=1.0, β::T=1.0) where {T<:Real} = BOAOB(h, zeros(SVector{3,T},N), β, exp(-h *γ ), sqrt( 1.0/β * (1-exp(-2*h*γ))))  
+# Constructor for BAOAB struct 
+BAOAB(h::T, V, at::AbstractAtoms ; γ::T=1.0, β::T=1.0) where {T<:Real} = BAOAB(h, forces(V, at), β, exp(-h *γ ), sqrt( 1.0/β * (1-exp(-2*h*γ))))
 
-function step!(s::BAOAB, V, at::AbstractAtoms )
+function step!(s::BAOAB, V, at::AbstractAtoms)
     B_step!(s, at; hf=.5)
     A_step!(s, at; hf=.5)
     O_step!(s, at)
     A_step!(s, at; hf=.5)
-    s.forces = forces(V, at)
+    s.F = forces(V, at)
     B_step!(s, at; hf=.5)
 end
 
+#at.pbc = (false,false, false)
+#sum(sum(-f .* x for (f, x) in zip(F[t], at.X[t]))/(3*N), t=1:Nsteps)/Nsteps ≈ 1/β        # Configurational temperature 
 
-at.pbc = (false,false, false)
-sum(sum(-f .* x for (f, x) in zip(F[t], at.X[t]))/(3*N), t=1:Nsteps)/Nsteps ≈ 1/β        # Configurational temperature 
-
-sum(sum(at.X[t] .* F[t])/(3*N), t=1:Nsteps)/Nsteps ≈ 1/β        # Configurational temperature 
+#sum(sum(at.X[t] .* F[t])/(3*N), t=1:Nsteps)/Nsteps ≈ 1/β        # Configurational temperature 
 # As step size tends to 0, the difference between the two above should tend to 0
 
+end
--- tests/test_fit.jl
+++ tests/test_fit.jl
@@ -19,7 +19,7 @@ end
 maxdeg = 4 # max degree
 ord = 2 # max body order correlation 
 Bsel = SimpleSparseBasis(ord, maxdeg)
-rcut = rnn(:Ald) # Cutoff radius
+rcut = 2.7*rnn(:Al) # Cutoff radius
 
 # Create phi_mnl one particle basis
 B1p = ACE.Utils.RnYlm_1pbasis(; maxdeg=maxdeg, Bsel = Bsel, 
@@ -30,7 +30,7 @@ basis1 = ACE.SymmetricBasis(ACE.Invariant(), B1p, Bsel)
 basis2 = ACE.SymmetricBasis(ACE.Invariant(), B1p, Bsel)
 
 # Create bulk configuration
-at = bulk(:Ti, cubic=true) * 3      
+at = bulk(:Al, cubic=true) * 3      
 
 rattle!(at,0.1)                     
 
@@ -41,14 +41,15 @@ fsmodel = FSModel(basis1, basis2,
                     x -> -sqrt(x), 
                     x -> 1 / (2 * sqrt(x)), 
                     ones(length(basis1)), ones(length(basis2)))
+r0 = rnn(:Al)
+Vpair = JuLIP.morse(;A=4.0, e0=.5, r0=r0, rcut=(1.9*r0, rcut))
 
-Vpair = JuLIP.morse(;A=4.0, e0=.5, r0=r0, rcut=(1.9*r0, 2.7*r0))
 
 model = CombModel([fsmodel, Vpair])
 
 
 #%%
-using BayesianMLIP.Dynamics: run!, BAOAB
+using BayesianMLIP: run!, BAOAB
 at = bulk(:Al, cubic=true) * 3
 N_obs = 10
 h = 0.01
@@ -56,11 +57,15 @@ N = length(at)
 ld = BAOAB(h, N; γ=1.0, β=1.0)
 data = []
 for k = 1:N_obs
-    for t = 1:nsteps
-        run!(ld, model, at, 1000)
-        push!(data,(at=deepcopy(at),E = energy(fsmodel,at), F = forces(fsmodel,at) ))
-    end
+    run!(ld, model, at, 1000)
+    push!(data,(at=deepcopy(at),E = energy(fsmodel,at), F = forces(fsmodel,at) ))
 end
+
+# 1) Check that Hamiltonian dynamics preservers total energy of mores model
+# 2) check that combined model works... 
+# 3) generate data using the combined model
+
+
 
 # Define Likelihood for isotropic Gaussian noise 
 # Define prior 
