e9fb23f9ad944ee5872122d7bc2801114340151e
Muchang Bahng <muchang.bahng@duke.edu>
2022-06-26 23:42:18 +0900

    BAOAB using Energy Only Potential

--- examples/run_nlmodel.jl
+++ examples/run_nlmodel.jl
@@ -17,17 +17,13 @@ using BayesianMLIP.ACEflux: FluxPotential
 
 using Test
 
-function createFSmodel()
+function createFSmodel(maxdeg, ord)
     # construct two bases
-    maxdeg = 6
-    ord = 2
     Bsel = SimpleSparseBasis(ord, maxdeg)
     B1p = ACE.Utils.RnYlm_1pbasis(; maxdeg=maxdeg)
     φ = ACE.Invariant()
     basis1 = SymmetricBasis(φ, B1p, O3(), Bsel)
 
-    maxdeg = 6
-    ord = 2
     Bsel = SimpleSparseBasis(ord, maxdeg)
     B1p = ACE.Utils.RnYlm_1pbasis(; maxdeg=maxdeg)
     φ = ACE.Invariant()
@@ -44,33 +40,15 @@ function createFSmodel()
 
     rcut = 2*rnn(:Al)
 
-
     model = CombPotential(FluxPotential(nlm1, rcut),FluxPotential(nlm2, rcut));# this is the FS-model
     return model
 end 
 
-model = createFSmodel()
+model = createFSmodel(6, 2)
+c = params(model)
 
-#extract parameters from model:
-c12a = params(model)
-set_params!(model,c12a)
-
-# test whether pack and unpack routines work as intended
-c12 = pack(model,svecs2vec(c_m1),svecs2vec(c_m2))
-c12b = pack(model,c_m1,c_m2)
-@test c12 == c12b
-c1, c2 = unpack(model, c12)
-@test c_m1 == c1 && c2 == c_m2
-
-
-# test energy and force evaluations
 at = bulk(:Al, cubic=true) * 2 
 rattle!(at,.5)
-E1 = energy(model,at)
-F1 = forces(model,at)
-rattle!(at,.1)
-F2 = forces(model,at)
-
 
 using Distributions
 using Distributions: logpdf, MvNormal
@@ -79,8 +57,7 @@ using BayesianMLIP.NLModels: params
 
 sampler = BAOAB(0.001, model, at)
 
-
-Ndata = 5
+Ndata = 100
 data = []
 for k = 1:Ndata
     run!(sampler, model, at, 1000; outp=nothing)
@@ -92,36 +69,35 @@ end
 w0 = 1.0 
 weight_E, weight_F = w0, w0/ (3*length(at)) # weights correspond to precision of noise
 
-# Log likelihood (Negative cost) for single data point
-log_likelihood = (model, d) -> -weight_E * abs(d.E - energy(model,d.at)) -  weight_F * sum(sum(abs2, g.rr - f.rr) 
-                     for (g, f) in zip(forces(model, d.at), d.F))
+# Log likelihood (Negative cost) for single data point, defined to be the sum of the squares of the elementwise differences
+# Modification: Likelihood function only includes energy observations 
+log_likelihood = (model, d) -> -weight_E * (d.E - energy(model,d.at))^2
 
 # Define prior 
-prior = MvNormal(zeros(length(c12)),I) 
+prior = MvNormal(zeros(length(c)),I) 
 
 # Define log posterior using formula p(θ | D ) ∝ p(D | θ) p(θ) = p(θ) ∏_i^N p(d_i | θ) ⟹ ...
 function log_posterior(model, data, prior, log_likelihood)
-    return sum(log_likelihood(model, d) for d in data) + logpdf(prior, params(model))
+    return sum(log_likelihood(model, d) for d in data) + logpdf(prior, c)
 end
 
-function U(θ) 
+function U(θ)
     set_params!(model, θ) 
     return -log_posterior(model, data, prior, log_likelihood)
 end 
 
-U(randn(length(c12)))
+theta = randn(72)
+U(theta)
 
-# posterior(θ) = exp( log_posterior(θ) ) 
-# U(θ) = - log_posterior(θ), exp(-U(θ)) = posterior(θ)
+using Zygote
+using ForwardDiff
+# Need to run BAOAB on potential U 
+inp = ones(72)
+U(inp)
+Zygote.gradient((x...) -> sum([i^2 for i in x]), inp...)
+Zygote.gradient(U, inp)
 
-# In case of a symmetric proposal the MH acceptance probability is
-# accept(θ,θ') = min(posterior(θ')/posterior(θ),1)
-# u ∼ Uniform([0,1]), log(u) < min[ 0, log_posterior(θ) - log_posterior(θ') ]
 
-# typical steps to evaluate the log-posterior 
-set_params!(model,randn(length(c12)));
-log_posterior(model, data, prior, log_likelihood)
-@time log_posterior(model, data, prior, log_likelihood) # Speed quite slow... Can we optimize this?
 
 using BayesianMLIP.MHoutputschedulers
 using BayesianMLIP.Samplers 
