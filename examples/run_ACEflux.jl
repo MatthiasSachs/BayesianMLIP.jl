124aabbcf040d22ffdd8dc4d59ea3a9457942c8d
Matthias Sachs <m.sachs@bham.ac.uk>
2022-07-05 14:13:45 +0100

    update

---
+++ examples/run_ACEflux.jl
@@ -0,0 +1,67 @@
+using ACEflux, JuLIP, ACE, Flux, Zygote
+using ACE: State
+using LinearAlgebra
+using Printf
+using Test, ACE.Testing
+using ACEbase
+
+using ACEflux: Linear_ACE
+using StaticArrays
+using JLD2
+#model = Chain(Linear_ACE(2, 7, 4), Dense(4, 3, σ), Dense(3, 1), sum)
+FS(ϕ) = ϕ[1] + sqrt(abs(ϕ[2]) + 1/100) - 1/10
+
+model = Chain(Linear_ACE(;ord = 2, maxdeg = 4), GenLayer(FS), sum)
+pot = FluxPotential(model, 6.0) 
+##
+
+# # we only check the derivatives of the parameters in the linear ace layer
+# # we do finite difference on the whole function, but only compare ACE parameters
+
+@info "dEnergy, dE/dP"
+
+at = bulk(:Cu, cubic=true) * 3
+rattle!(at,0.6) 
+
+
+theta = load_object("Data/dataset1.jld2")["theta"];
+data = load_object("Data/dataset1.jld2")["data"];
+
+w0 = 1.0 
+weight_E, weight_F = w0, w0/ (3*length(at)) # weights correspond to precision of noise
+log_likelihood = (model, d) -> -weight_E * abs(d.E - energy(model,d.at)) -  weight_F * sum(sum(abs2, g - f.rr) 
+                     for (g, f) in zip(forces(model, d.at), d.F))
+#log_likelihood_Energy = (model, d) -> -1.0 * (d.E - energy(model,d.at))^2
+
+
+# Much of the computing power is used to solve for the force part of log_likelihood
+
+using Distributions
+using BayesianMLIP.Utils: StatisticalModel, get_params
+
+priorNormal = MvNormal(zeros(length(get_params(model))),I)
+
+sm = StatisticalModel(
+    log_likelihood, 
+    priorNormal, 
+    pot, 
+    data
+);
+
+using BayesianMLIP.Utils: get_glp, get_glpr, get_gll
+gll = get_gll(sm)
+glpr = get_glpr(sm)
+glp = get_glp(sm)
+
+
+c = zeros(Flux.params(sm.model))
+c =rand(length(c))
+
+gll(c,data[2])
+n = 50
+g = glp(c,data)
+
+
+
+
+
--- src/utils.jl
+++ src/utils.jl
@@ -6,6 +6,8 @@ using LinearAlgebra
 using BayesianMLIP.Outputschedulers
 using BayesianMLIP.NLModels
 
+using Flux, FluxOptTools, Zygote, ACEflux
+
 export animation, StatisticalModel, log_posterior
 
 mutable struct StatisticalModel 
@@ -15,10 +17,108 @@ mutable struct StatisticalModel
     data
 end 
 
+function Flux.params(pot::ACEflux.FluxPotential) 
+    return Flux.params(pot.model) 
+end
+
+function Flux.params(s::StatisticalModel) 
+    return Flux.params(s.model) 
+end
+
+nparams(m::StatisticalModel) = nparams(m.model)
+
+function log_likelihood(m::StatisticalModel)
+    return sum(m.log_likelihood(m.model, d) for d in m.data)
+end
+
+# function log_prior(m::StatisticalModel)
+#     return logpdf(m.prior, θ)
+# end
+
+log_posterior(m::StatisticalModel) = sum(m.log_likelihood(m.model, d) for d in m.data) #+ m.log_prior(Flux.params(m.model))
+
 function log_posterior(m::StatisticalModel, θ) 
     set_params!(m.model, θ)
-    return sum(m.log_likelihood(m.model, d) for d in m.data) + logpdf(m.prior, θ)
+    return log_posterior(m)
 end
+
+function get_gll(sm::StatisticalModel)
+    function gll(c,d) 
+        set_params!(sm.model,c)
+        p = Flux.params(sm.model)
+        dL = Zygote.gradient(()->sm.log_likelihood(sm.model, d), p)
+        gradvec = zeros(p)
+        copy!(gradvec,dL)
+        return(gradvec)
+    end
+    return gll
+end
+
+function get_glpr(sm::StatisticalModel)
+    function glpr(c::AbstractArray{T}) where {T<:Real} 
+        return Zygote.gradient(c->logpdf(sm.prior,c), c)[1]
+    end
+    return glpr
+end
+
+function get_glp(sm::StatisticalModel)
+    gll, glpr =  get_gll(sm), get_glpr(sm)
+    return get_glp(gll, glpr)
+end
+
+function get_glp(gll, glpr)
+    function glp(c::AbstractArray, batch)
+        return sum(gll(c,d) for d in batch) + glpr(c)
+    end
+    return glp
+end
+
+
+
+# function grad_log_posterior!(grad::AbstractArray{T} , m::StatisticalModel, θ::AbstractArray{T}) where {T <:Real}
+#     set_params!(m.model, θ)
+#     p = Flux.params(m.model)
+#     print(nparams(m))
+#     dL = Zygote.gradient(()->log_posterior(m), p)
+#     copy!(grad, dL)
+# end
+
+# function grad_log_posterior(m::StatisticalModel, θ::AbstractArray{T}) where {T <:Real}
+#     grad = zeros( nparams(m))
+#     grad_log_posterior!(grad, m, θ)
+#     return grad
+# end
+
+
+
+
+
+# Wrapper functions for FluxPotentials
+nparams(pot::FluxPotential) = nparams(pot.model)
+function nparams(model::Chain)
+    return sum(length(p) for p in Flux.params(model))
+end
+
+params(pot::FluxPotential)  = Flux.params(pot.model)
+
+
+function get_params(m)
+    c = zeros(nparams(m))
+    get_params!(c, m) 
+end
+get_params!(c::AbstractArray{T}, pot::FluxPotential)  where {T <: Real} = get_params!(c,pot.model)
+get_params!(c::AbstractArray{T}, model::Chain)  where {T <: Real} = copy!(c,Flux.params(model))
+
+set_params!(pot::FluxPotential, c::AbstractArray{T}) where {T <: Real} =  set_params!(pot.model, c) 
+
+function set_params!(model::Chain, c::AbstractArray{T}) where {T <: Real}
+    p = Flux.params(model)
+    copy!(p,c) 
+end
+
+
+
+
 
 # animation 
 
@@ -37,6 +137,9 @@ function animation(outp::atoutp ; name::String="anim", trace=false)
     end
     gif(anim, "$(name).mp4", fps=200)
 end
+
+
+
 
 
 # using Plots
