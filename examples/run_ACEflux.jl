3598dd5ee09dbced35fe72b08905fa2814b1b180
Matthias Sachs <m.sachs@bham.ac.uk>
2022-07-21 12:51:07 +0100

    non-pushed updates

--- examples/run_ACEflux.jl
+++ examples/run_ACEflux.jl
@@ -11,7 +11,7 @@ using JLD2
 #model = Chain(Linear_ACE(2, 7, 4), Dense(4, 3, σ), Dense(3, 1), sum)
 FS(ϕ) = ϕ[1] + sqrt(abs(ϕ[2]) + 1/100) - 1/10
 
-model = Chain(Linear_ACE(;ord = 2, maxdeg = 4), GenLayer(FS), sum)
+model = Chain(Linear_ACE(;ord = 4, maxdeg = 6), GenLayer(FS), sum)
 pot = FluxPotential(model, 6.0) 
 ##
 
@@ -51,14 +51,35 @@ data[1].E
 gll(c,data[1])
 
 
-glp(c,data[1:2])
-n = 10
+glp(c,data[1:10])
+n = 1
+c =rand(length(c))
+@time glp(c,data[1:n])
+
 
 g_estimate = glp(c,data[1:n])/n
 
 glp(c,data)
 
+using StatsBase
+data_size = length(data)
+mb_size = 1
+mbatch = sample(1:data_size, mb_size, replace = false)
+estimated_grad = data_size/mb_size * sum(gll(c,data[i]) for i in mbatch) + glpr(c)
 
 
 
+c =rand(length(c))
+loss_vec = [ (mbatch = sample(1:data_size, mb_size, replace = false); data_size/mb_size * sum(gll(c,data[i]) for i in mbatch) + glpr(c)) for _ in 1:100]
+std_vec = std(loss_vec)
+mean_vec  = abs.(mean(loss_vec))
 
+using Plots
+Plots.plot(std_vec./mean_vec; yscale = :log)
+Plots.plot(std_vec; yscale = :log)
+
+using BayesianMLIP.Utils: set_params!
+c = rand(length(c))
+set_params!(sm.model, c)
+@time sum(log_likelihood(sm.model,d) for d in data)
+@time glp(c,data)
