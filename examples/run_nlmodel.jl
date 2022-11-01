249601939f22333a09572a7574c9170ff188fc64
Muchang Bahng muchang.bahng@duke.edu <mbahng@r8-ortner2.math.ubc.ca>
2022-10-31 18:31:17 -0700

    checkpoint before BAOAB implementation

--- examples/run_nlmodel.jl
+++ examples/run_nlmodel.jl
@@ -11,7 +11,7 @@ log_likelihood_L2(pot::ACEflux.FluxPotential, d; ωE = 1.0, ωF = ωE/(3*length(
 log_likelihood_Null = ConstantLikelihood() 
 priorNormal = MvNormal(zeros(nparams(pot)), I)
 priorUniform = FlatPrior()
-real_data = getData(JSON.parsefile("/z1-mbahng/mbahng/mlearn/data/Cu/training.json"))[1:5] ; # 262-vector 
+real_data = getData(JSON.parsefile("./Run_Data/Real_Data/training_test/Cu/training.json"))[1:5] ; # 262-vector 
 
 
 # stm1 = StatisticalModel(log_likelihood_Null, priorNormal, pot, real_data) ;
@@ -20,8 +20,13 @@ stm = StatisticalModel(log_likelihood_L2, priorUniform, pot, real_data) ;
 # Run MCMC to get to minimum, and then run SGLD (linear parameters) 
 st1 = State_θ(zeros(6)) ; 
 outp1 = MHoutp_θ() ; 
-s1 = nonlinearMetropolis(1e1, st1, stm; α=0.9);
+s1 = linearMetropolis(1e1, st1, stm; α=0.9);
 conditionalSamplers.run!(st1, s1, stm, 10000, outp1)
+Summary(outp1)
+Histogram(outp1)
+Trajectory(outp1) 
+mean(outp1.θ)
+
 
 st2 = State_θ(mean(outp1.θ)) ;
 outp2 = MHoutp_θ() ; 
@@ -46,21 +51,31 @@ Histogram(outp4)
 Trajectory(outp4) 
 
 
-
-st5 = State_θ(zeros(6)) ; 
+st5 = State_θ(zeros(nparams(stm))) ; 
 outp5 = MHoutp_θ() ; 
-s5 = GibbsSampler(linearMetropolis(1e1, st5, stm; α=0.9), nonlinearMetropolis(1e1, st5, stm; α=0.9), 1.);
-@time conditionalSamplers.run!(st5, s5, stm, 10000, outp5)
+s5 = GibbsSampler(
+    linearMetropolis(1e1, st5, stm; α=0.9), 
+    nonlinearMetropolis(1e1, st5, stm; α=0.6), 
+    0.5
+);
+conditionalSamplers.run!(st5, s5, stm, 50000, outp5)
+Summary(outp5)
+Histogram(outp5)
+Trajectory(outp5)
+
 
 st6 = State_θ(mean(outp5.θ)) 
 outp6 = MHoutp_θ() ; 
-s6 = GibbsSampler(linearMetropolis(1e1, st6, stm; α=0.9), nonlinearSGLD(4e-8, st6, stm, 1; α=0.9), .5);
-@time conditionalSamplers.run!(st6, s6, stm, 1000, outp6)
+s6 = GibbsSampler(
+    linearMetropolis(1e1, st6, stm; α=0.9), 
+    nonlinearSGLD(1e-7, st6, stm, 1; α=0.9), 
+    .5
+);
+conditionalSamplers.run!(st6, s6, stm, 50000, outp6)
 
 Summary(outp6)
 Histogram(outp6)
 Trajectory(outp6)
-
-A = randn(5000, 5000) 
-B = randn(5000, 5000) 
-@time A * B
+Summary(outp6, save_fig=true, title="summary")
+Histogram(outp6, save_fig=true, title="histogram")
+Trajectory(outp6, save_fig=true, title="trajectory") 
